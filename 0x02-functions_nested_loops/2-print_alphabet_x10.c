b7e1f8212a0b4b47fb35794dce9f6e13818abc1b
Zeezbaba <Azeez_okeleji>
2023-08-15 05:17:53 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,17 @@
  */
 
 void print_alphabet_x10(void)
+
 {
 
 	char alp;
+
 	int i;
+
 	i = 0;
+
 	while (i < 10)
+
 	{
 		for (alp = 'a'; alp <= 'z'; alp++)
 		{
@@ -20,4 +25,5 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 		i++;
 	}
+
 }
