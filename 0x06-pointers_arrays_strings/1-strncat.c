673c7d72c3ab18bab0fc8c992b051b3215811627
Zeezbaba <Azeez_okeleji>
2023-08-23 12:33:29 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncat - function that concatenates two strings
+ * @src: source string pointer
+ * @dest: destination string pointer
+ * @n: number of bytes to be concetenated
+ * Return: destination string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int lenght_of_string, z;
+	/* z is a counter for n bytes of src to be concatenated */
+	/* lenght_of_string = lenght of destination string */
+
+	lenght_of_string = 0;
+	while (dest[lenght_of_string] != '\0')
+	{
+		lenght_of_string++;
+	}
+	for (z = 0; z < n && src[z] != '\0'; z++, lenght_of_string++)
+	{
+		dest[lenght_of_string] = src[z];
+	}
+	dest[lenght_of_string] = '\0';
+	return (dest);
+}
