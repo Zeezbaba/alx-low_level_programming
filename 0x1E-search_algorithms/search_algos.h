c9a9063264ed031623ad2d6a8d9fd851937c88b2
Zeezbaba <Azeez_okeleji>
2024-03-11 19:22:25 -0700

    Exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,8 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int search_binary(int *array, int value, size_t low, size_t high);
+size_t min(size_t i, size_t j);
 /* listint_t *jump_list(listint_t *list, size_t size, int value); */
 
 #endif
